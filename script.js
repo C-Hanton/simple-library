0eddd28baa5ba9c8e59ebf6c1d4c1147e8375cc6
Conor_H <130234528+C-Hanton@users.noreply.github.com>
2024-12-29 14:23:36 +0000

    added library array and commented out constructor

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Excercise is to write a cosntruction to make a Book object
 //We will revisit this in the next project. Your book objects should have the book’s title, author, the number of pages, and whether or not you have read the book.
 
-function Book(title, author, pages, read) {
+/*function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -10,5 +10,10 @@ function Book(title, author, pages, read) {
         return `${this.title} by ${this.author} has ${this.pages} pages and is ${this.read}`
     }
 }
+    */
 
 const theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 295, "read");
+
+// Project Start
+
+const myLibrary = [];
